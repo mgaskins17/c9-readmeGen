8104a7bf0ba73e012e5ab774a9890cc74117b134
EfSoren <ef.sorensen@me.com>
2022-11-09 11:23:15 -0700

    First Trail run

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# c9-readmeGen
+# a
+        
+        ## Description
+        b
+        
+        ## Table of Contents
+        Add links here
+        
+        ##Installation
+        d
+        
+        ## Usage
+        s
+        
+        ## License
+        None
+        
+        ## Contributing
+        d
+        
+        ## Tests
+        s
+        
+        ## Questions
+        Please refer to either my github or email with questions
+        g
+        https://github.com/a 
+        
--- template.js
+++ template.js
@@ -1,1 +1,86 @@
+function writeTemplate(){
+
+const rmTemplate = 
+`# ${title}
+
+ ## Description
+${description}
+
+## Table of Contents
+Add links here
+
+##Installation
+${installation}
+
+## Usage
+${usage}
+
+## License
+${license}
+
+## Contributing
+${contributing}
+
+## Tests
+${tests}
+
+## Questions
+Please refer to either my github or email with questions
+${email}
+https://github.com/${github} 
+`
+console.log(rmTemplate);
+}
+/* const promptQuestions = [
+    {
+        type: 'input',
+        message: 'What is your project title.',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please enter a description of your projects purpose.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please enter installation guidelines.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please describe how to use your project.',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'What type of license do you have?',
+        choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'Please list guidelines for contributing to your project',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'placeholder',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your github username',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email address',
+        name: 'email',
+    },
+    
+] */
+module.exports = {writeTemplate}
+
+
 
